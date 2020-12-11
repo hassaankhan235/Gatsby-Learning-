dd0c3b94ba00319f56da87b29ab21ee4b03b9735
hassaankhan235 <hassaankhan235@gmail.com>
2020-12-11 15:56:19 +0300

    Create and search Indexes

---
+++ faunadb/crIndex.js
@@ -0,0 +1,28 @@
+const faunadb = require('faunadb'),
+q = faunadb.query;
+
+(async () => {
+    const client = new faunadb.Client({secret: 'fnAD8k-3RiACB8K1iVEw5OgNm-JRLgWZHOuYAd7v'})
+
+    try{
+        // Create Index in Faunadb
+        // const res = await client.query(
+        //     q.CreateIndex({
+        //         name : 'postTitles',
+        //         source: q.Collection('posts'),
+        //         terms : [{field : ['data', 'title']}]
+        //     })
+        // )
+        // console.log('Index Created' + res.name)
+
+        
+        // Search by Index 
+        const res = await client.query(
+            q.Get(q.Match(q.Index('postTitles'), 'Hassaan is the greatest programmer of the age'))
+        )
+        console.log('Result', res)
+    }
+    catch(error){
+        console.log("Error",error)
+    }
+})()
